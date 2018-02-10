cfb3fb5870b6b11fdd605dab3e115ca2b54feb83
Mian Uddin <mianuddin@gmail.com>
2018-02-10 12:35:23 -0800

    feat(assets, layouts, utils, gatsby-config): add Avenir

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
       resolve: 'gatsby-plugin-typography',
       options: {
         pathToConfigModule: 'src/utils/typography.js',
+        omitGoogleFont: true,
       },
     },
     'gatsby-plugin-react-helmet',
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,5 +1,19 @@
 import Typography from 'typography';
 
-const typography = new Typography({ baseFontSize: '16px' });
+const typography = new Typography({
+  baseFontSize: '16px',
+  headerFontFamily: [
+    'Avenir',
+    'Helvetica Neue',
+    'Helvetica',
+    'sans-serif',
+  ],
+  bodyFontFamily: [
+    'Avenir',
+    'Helvetica Neue',
+    'Helvetica',
+    'sans-serif',
+  ],
+});
 
 export default typography;
