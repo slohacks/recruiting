d1225228238ab905e9f012e24fbde6ca6e8b5bed
Mian Uddin <mianuddin@gmail.com>
2018-02-10 01:18:41 -0800

    refactor(pages/index): refactor inline styles and JSX
    * Move inline styles to CSS module
    * Move JSX attribute(s) to one line where applicable

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,69 +1,29 @@
 import React from 'react';
+import styles from './index.module.css';
 import logo from '../assets/logo.png';
-import teamWhite from '../assets/teamWhite.jpg';
-import teamColor from '../assets/teamColor.jpg';
 import animatedTeam from '../assets/team.gif';
 
 const IndexPage = () => (
   <div>
-    <header
-      style={{
-        width: '100vw',
-        height: '100vh',
-        background: `url(${teamWhite})`,
-        backgroundSize: 'cover',
-        backgroundAttachment: 'fixed',
-        display: 'grid',
-        gridTemplateColumns: '9.58% [logo] 9.84% 20.99% [content] 50% 9.58%',
-        gridTemplateRows: '12.9% [logo]12% 6.5% [content]55% 12.9%',
-      }}
-    >
+    <header className={styles.hero}>
       <img
         src={logo}
         alt="Logo"
-        style={{
-          gridColumnStart: 'logo',
-          gridRowStart: 'logo',
-        }}
+        className={styles.logo}
       />
-      <h1
-        style={{
-          display: 'none',
-        }}
-      >
+      <h1 className={styles.h1}>
         SLO Hacks
       </h1>
-      <h2
-        style={{
-          gridColumnStart: 'content',
-          gridRowStart: 'content',
-        }}
-      >
+      <h2 className={styles.recruitingText}>
         We&apos;re recruiting!
       </h2>
       <img
         src={animatedTeam}
         alt="SLO Hacks Team"
-        style={{
-          gridColumnStart: 'logo',
-          gridColumnEnd: 4,
-          gridRowStart: 'content',
-          alignSelf: 'end',
-          marginBottom: 0,
-          maxHeight: '100%',
-          maxWidth: '100%',
-        }}
+        className={styles.animatedTeam}
       />
     </header>
-    <div
-      style={{
-        width: '100vw',
-        height: '20vh',
-        background: `url(${teamColor})`,
-        backgroundSize: 'cover',
-        backgroundAttachment: 'fixed',
-      }}
-    />
+    <div className={styles.spacer} />
     <div
       style={{
         width: '100vw',
