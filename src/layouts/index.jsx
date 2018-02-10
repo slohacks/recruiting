3babc1d78f5f60ceac41f64ac9fc829280cdf859
Mian Uddin <mianuddin@gmail.com>
2018-02-09 16:24:42 -0800

    refactor(pages, components): remove extraneous content

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -2,27 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 
-import Header from '../components/Header';
 import './index.css';
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="SLO Hacks"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: 'SLO Hacks' },
+        { name: 'keywords', content: 'san luis obispo, cal poly, hackathon, coding, software, competition, recruiting' },
       ]}
     />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <div>
       {children()}
     </div>
   </div>
