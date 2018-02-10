1fcd1cbb22f269cd62a674cce730299fcd4ff86c
Mian Uddin <mianuddin@gmail.com>
2018-02-09 23:31:03 -0800

    feat(pages/index): add team color reveal on scroll

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,57 +1,76 @@
 import React from 'react';
 import logo from '../assets/logo.png';
-import background from '../assets/background.jpg';
+import teamWhite from '../assets/teamWhite.jpg';
+import teamColor from '../assets/teamColor.jpg';
 import animatedTeam from '../assets/team.gif';
 
 const IndexPage = () => (
-  <header
-    style={{
-      width: '100vw',
-      height: '100vh',
-      background: `url(${background})`,
-      backgroundSize: 'cover',
-      display: 'grid',
-      gridTemplateColumns: '9.58% [logo] 9.84% 20.99% [content] 50% 9.58%',
-      gridTemplateRows: '12.9% [logo]12% 6.5% [content]55% 12.9%',
-    }}
-  >
-    <img
-      src={logo}
-      alt="Logo"
+  <div>
+    <header
       style={{
-        gridColumnStart: 'logo',
-        gridRowStart: 'logo',
-      }}
-    />
-    <h1
-      style={{
-        display: 'none',
+        width: '100vw',
+        height: '100vh',
+        background: `url(${teamWhite})`,
+        backgroundSize: 'cover',
+        backgroundAttachment: 'fixed',
+        display: 'grid',
+        gridTemplateColumns: '9.58% [logo] 9.84% 20.99% [content] 50% 9.58%',
+        gridTemplateRows: '12.9% [logo]12% 6.5% [content]55% 12.9%',
       }}
     >
-      SLO Hacks
-    </h1>
-    <h2
+      <img
+        src={logo}
+        alt="Logo"
+        style={{
+          gridColumnStart: 'logo',
+          gridRowStart: 'logo',
+        }}
+      />
+      <h1
+        style={{
+          display: 'none',
+        }}
+      >
+        SLO Hacks
+      </h1>
+      <h2
+        style={{
+          gridColumnStart: 'content',
+          gridRowStart: 'content',
+        }}
+      >
+        We&apos;re recruiting!
+      </h2>
+      <img
+        src={animatedTeam}
+        alt="SLO Hacks Team"
+        style={{
+          gridColumnStart: 'logo',
+          gridColumnEnd: 4,
+          gridRowStart: 'content',
+          alignSelf: 'end',
+          marginBottom: 0,
+          maxHeight: '100%',
+          maxWidth: '100%',
+        }}
+      />
+    </header>
+    <div
       style={{
-        gridColumnStart: 'content',
-        gridRowStart: 'content',
-      }}
-    >
-      We&apos;re recruiting!
-    </h2>
-    <img
-      src={animatedTeam}
-      alt="SLO Hacks Team"
-      style={{
-        gridColumnStart: 'logo',
-        gridColumnEnd: 4,
-        gridRowStart: 'content',
-        alignSelf: 'end',
-        marginBottom: 0,
-        maxHeight: '100%',
-        maxWidth: '100%',
+        width: '100vw',
+        height: '20vh',
+        background: `url(${teamColor})`,
+        backgroundSize: 'cover',
+        backgroundAttachment: 'fixed',
       }}
     />
-  </header>
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+      }}
+    />
+  </div>
 );
 
 export default IndexPage;
