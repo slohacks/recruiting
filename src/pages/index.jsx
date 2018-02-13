0bf0f684d1366b46a969a3080d6dc3da0c15483d
Mian Uddin <mianuddin@gmail.com>
2018-02-12 21:50:27 -0800

    fix(pages/index): fix typo in copy

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -40,7 +40,7 @@ const IndexPage = ({ data }) => (
     <section className={styles.content}>
       <div className={styles.text}>
         <h2>Who are we?</h2>
-        <p>The SLO Hacks team is dedicated to spreading a culture of tech and innovation throughout Cal Poly. Aside from our flagship event, we host multiple hackathons each year. Our team is composed of six groups: Design, Hacker Experience, Marketing, Sponsorship, and Tech.</p>
+        <p>The SLO Hacks team is dedicated to spreading a culture of tech and innovation throughout Cal Poly. Aside from our flagship event, we host multiple hackathons each year. Our team is composed of five groups: Design, Hacker Experience, Marketing, Sponsorship, and Tech.</p>
         <h2>Why should you join?</h2>
         <p>As a hackathon organizer, you’ll gain valuable experience by working on amazing events. Our team members work in a variety of areas, they interact with great sponsors from companies like Viasat, Google, and Intuit, create open source tools to empower our events, plan logistics for events comprising hundreds of people, and much much more.</p>
       </div>
